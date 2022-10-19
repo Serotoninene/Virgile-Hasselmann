43132ef6d32a34fb978e14f31b2a3f1cff3d545b
Serotoninene <pujol.alexandre@hotmail.fr>
2022-10-19 11:40:49 +0200

    set up of the heroVideo component + rangement des fichiers assets + added the first video

---
+++ components/Home/HeroVideo.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function HeroVideo() {
+  return <div>HeroVideo</div>;
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import type { NextPage } from "next";
 // Components
 import DarkGradients from "components/Home/DarkGradients";
+import HeroVideo from "components/Home/HeroVideo";
 
 const Home: NextPage = () => {
   return (
     <div className="relative">
-      <DarkGradients />
+      <HeroVideo />
+      <DarkGradients /> {/* <-- must stay on the bottom of the component */}
     </div>
   );
 };
