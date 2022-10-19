800a5e473048876e83da76b58bcf0984fdaaaa05
Serotoninene <pujol.alexandre@hotmail.fr>
2022-10-19 12:13:19 +0200

    set up the video background on the herobanner of the main page

--- components/Home/HeroVideo.tsx
+++ components/Home/HeroVideo.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function HeroVideo() {
   return (
-    <div className="fixed h-screen w-screen">
-      <video width="100%" height="100%" loop autoPlay>
+    <div className="fixed h-screen w-screen overflow-hidden flex justify-center items-center">
+      <video loop autoPlay muted className="w-full h-full object-cover">
         <source src="/assets/videos/heroVideo.mp4" type="video/mp4" />
       </video>
     </div>
