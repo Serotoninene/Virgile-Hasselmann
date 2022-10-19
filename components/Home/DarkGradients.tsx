2dc11988f98e771f62097b0960830b2a97358bfa
Serotoninene <pujol.alexandre@hotmail.fr>
2022-10-19 11:51:15 +0200

    made dark gradients bigger | set up the heroVideo

--- components/Home/DarkGradients.tsx
+++ components/Home/DarkGradients.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function DarkGradients() {
   return (
     <div className="h-screen w-screen fixed flex flex-col justify-between pointer-events-none">
-      <div className="h-[10vh] bg-gradient-to-b from-black to-transparent"></div>
+      <div className="h-[20vh] bg-gradient-to-b from-black to-transparent"></div>
       <div className="h-[60vh] xs:h-[33vh] bg-gradient-to-t from-black to-transparent"></div>
     </div>
   );
--- components/Home/HeroVideo.tsx
+++ components/Home/HeroVideo.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function HeroVideo() {
-  return <div>HeroVideo</div>;
+  return (
+    <div className="fixed h-screen w-screen">
+      <video width="100%" height="100%" loop autoPlay>
+        <source src="/assets/videos/heroVideo.mp4" type="video/mp4" />
+      </video>
+    </div>
+  );
 }
