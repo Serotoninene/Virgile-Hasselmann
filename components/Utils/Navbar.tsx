347b9d082af4df80efad5990898ffcd82e4aeceb
Serotoninene <pujol.alexandre@hotmail.fr>
2022-10-19 18:01:22 +0200

    small animation added to the navbar

--- components/Utils/Navbar.tsx
+++ components/Utils/Navbar.tsx
@@ -2,12 +2,33 @@ import React, { useEffect, useState } from "react";
 // Nextjs
 import Link from "next/link";
 import { useRouter } from "next/router";
+// framer motion
+import { motion } from "framer-motion";
 
 const links = [
   { title: "Videos", href: "/videos" },
   { title: "Photos", href: "/photos" },
   { title: "Contact", href: "/contact" },
 ];
+
+const containerAnim = {
+  hidden: {},
+  shown: {
+    transition: {
+      staggerChildren: 0.02,
+      delayChildren: 1.4,
+    },
+  },
+};
+
+const itemsAnim = {
+  hidden: { y: -50, opacity: 0 },
+  shown: {
+    y: 0,
+    opacity: 1,
+    transition: { duration: 0.4, ease: [0.3, 0.01, -0.05, 0.95] },
+  },
+};
 
 export default function Navbar() {
   const { pathname } = useRouter();
@@ -19,23 +40,30 @@ export default function Navbar() {
   }, [pathname]);
 
   return (
-    <div className="flex justify-between py-2 px-4 sm:py-4 sm:px-6">
-      <div className="font-icon">
+    <motion.div
+      variants={containerAnim}
+      initial="hidden"
+      animate="shown"
+      exit="hidden"
+      className="flex justify-between py-2 px-4 sm:py-4 sm:px-6"
+    >
+      <motion.div variants={itemsAnim} className="font-icon">
         <Link href="/">VH</Link>
-      </div>
+      </motion.div>
       <ul className="hidden xs:flex">
         {isLinks &&
           links.map((link, idx) => (
-            <li
+            <motion.li
               key={idx}
+              variants={itemsAnim}
               className={`ml-14 text-lg hover:font-bold ${
                 pathname === link.href ? "font-bold" : "font-light"
               }`}
             >
               <Link href={link.href}>{link.title}</Link>
-            </li>
+            </motion.li>
           ))}
       </ul>
-    </div>
+    </motion.div>
   );
 }
