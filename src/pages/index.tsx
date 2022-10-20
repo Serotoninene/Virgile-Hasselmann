90b6e4461af0a7c561a3479228dbee1c7ccadb17
Serotoninene <pujol.alexandre@hotmail.fr>
2022-10-20 16:16:28 +0200

    added the useScrollBlock hook and useWindowSize | not really managed to deal with scrolling tho ...

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { RefObject, useEffect, useRef, useState } from "react";
 import type { NextPage } from "next";
 // Components
 import DarkGradients from "@src/components/Home/DarkGradients";
@@ -6,33 +6,44 @@ import HeroVideo from "@src/components/Home/HeroVideo";
 import MainMenu from "@src/components/Home/MainMenu";
 import { useScroll } from "framer-motion";
 import useDebounce from "@src/hooks/useDebounce";
+import useWindowSize from "@src/hooks/useWindowSize";
+import { useScrollBlock } from "@src/hooks/useScrollBlock";
 //
 
 const Home: NextPage = () => {
   const { scrollY } = useScroll();
+  const { height } = useWindowSize();
   const [goToMainMenu, setGoToMainMenu] = useState<boolean>(false);
+
+  // const scrollTo = (e: number) => {
+  //   blockScroll();
+  //   window.scrollTo({
+  //     top: height,
+  //   });
+  //   allowScroll();
+  // };
 
   useEffect(() => {
     return scrollY.onChange((latest) => {
       if (latest < 0) return;
 
-      scrollY.getPrevious() - latest < 0
-        ? setGoToMainMenu(true)
-        : setGoToMainMenu(false);
+      if (scrollY.getPrevious() - latest < 0) {
+        setGoToMainMenu(true);
+      } else {
+        setGoToMainMenu(false);
+      }
     });
   }, [scrollY]);
 
-  console.log(goToMainMenu);
-
   return (
-    <>
+    <div id="Home">
       <div className="fixed w-screen">
         <MainMenu goToMainMenu={goToMainMenu} />
         <HeroVideo />
         <DarkGradients /> {/* <-- must stay on the bottom of the component */}
       </div>
-      <div className="bg-slate-500 h-[150vh]" />
-    </>
+      <div className="bg-dark h-[105vh]" />
+    </div>
   );
 };
 
