717fc23580c0cb6a69e46f4ed7d239cc67bde327
Serotoninene <pujol.alexandre@hotmail.fr>
2022-10-24 14:27:48 +0200

    trying to fix mainMenu on mobile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState, WheelEvent } from "react";
+import { TouchEvent, useContext, useEffect, useState, WheelEvent } from "react";
 import type { NextPage } from "next";
 // Context
 import { CursorContext } from "@src/contexts/CursorProvider";
@@ -6,12 +6,33 @@ import { CursorContext } from "@src/contexts/CursorProvider";
 import DarkGradients from "@src/components/Home/DarkGradients";
 import HeroVideo from "@src/components/Home/HeroVideo";
 import MainMenu from "@src/components/Home/MainMenu";
+import useWindowSize from "@src/hooks/useWindowSize";
 
 const Home: NextPage = () => {
+  const { width } = useWindowSize();
   const { changeCursorType } = useContext(CursorContext);
   const [goToMainMenu, setGoToMainMenu] = useState<boolean>(false);
 
-  const triggerMainMenuAnim = (e: WheelEvent<HTMLDivElement>) => {
+  const triggerMainMenuAnimMobile = (e: TouchEvent<HTMLDivElement>) => {
+    if (width! < 768) return;
+
+    let lastY;
+    let currentY = e.touches[0].clientY;
+    if (lastY && currentY > lastY) {
+      // moved down
+      setGoToMainMenu(true);
+      changeCursorType("pointer");
+    } else if (lastY && currentY < lastY) {
+      // moved up
+      setGoToMainMenu(false);
+      changeCursorType("scrollIndicator");
+    }
+    lastY = currentY;
+  };
+
+  const triggerMainMenuAnimDesk = (e: WheelEvent<HTMLDivElement>) => {
+    if (width! > 768) return;
+
     if (e.deltaY > 0) {
       setGoToMainMenu(true);
       changeCursorType("pointer");
@@ -26,7 +47,12 @@ const Home: NextPage = () => {
   }, []);
 
   return (
-    <div id="Home" className="w-screen" onWheel={(e) => triggerMainMenuAnim(e)}>
+    <div
+      id="Home"
+      className="w-screen"
+      onWheel={(e) => triggerMainMenuAnimDesk(e)}
+      onTouchMove={(e) => triggerMainMenuAnimMobile(e)}
+    >
       <MainMenu goToMainMenu={goToMainMenu} />
       <HeroVideo />
       <DarkGradients /> {/* <-- must stay on the bottom of the component */}
