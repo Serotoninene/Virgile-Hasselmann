9236268890e92de68a364ce525b5e63bed072cc5
Serotoninene <pujol.alexandre@hotmail.fr>
2022-10-24 13:53:20 +0200

    replaced the complicated scroll event by an easy onWheel event

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState, WheelEvent } from "react";
 import type { NextPage } from "next";
-import { userAgentFromString } from "next/server";
 // framer motion
 import { useScroll } from "framer-motion";
 // Context
@@ -9,55 +8,31 @@ import { CursorContext } from "@src/contexts/CursorProvider";
 import DarkGradients from "@src/components/Home/DarkGradients";
 import HeroVideo from "@src/components/Home/HeroVideo";
 import MainMenu from "@src/components/Home/MainMenu";
-// Custom Hook
-import useWindowSize from "@src/hooks/useWindowSize";
 
 const Home: NextPage = () => {
-  // Safari 3.0+ "[object HTMLElementConstructor]"
-  const userAgent = userAgentFromString(undefined);
-  const isSafariUsed = userAgent.browser.name === "Safari";
-  console.log(userAgent);
-
   const { scrollY } = useScroll();
-  const { width } = useWindowSize();
   const { changeCursorType } = useContext(CursorContext);
   const [goToMainMenu, setGoToMainMenu] = useState<boolean>(false);
+
+  const triggerMainMenuAnim = (e: WheelEvent<HTMLDivElement>) => {
+    if (e.deltaY > 0) {
+      setGoToMainMenu(true);
+      changeCursorType("pointer");
+    } else {
+      setGoToMainMenu(false);
+      changeCursorType("scrollIndicator");
+    }
+  };
 
   useEffect(() => {
     changeCursorType("scrollIndicator");
   }, []);
 
-  useEffect(() => {
-    return scrollY.onChange((latest) => {
-      if (latest < 0) return;
-
-      let direction = scrollY.getPrevious() - latest; // <- from top to bottom on other browsers thant safari
-      // if we're on safari, weirdly enought we have to reverse the order of the scroll so it's from top to bottom
-      isSafariUsed &&
-        width! < 768 &&
-        (direction = latest - scrollY.getPrevious());
-
-      if (direction < 0) {
-        setGoToMainMenu(true);
-        changeCursorType("pointer");
-      } else {
-        setGoToMainMenu(false);
-        changeCursorType("scrollIndicator");
-      }
-    });
-  }, [scrollY]);
-
   return (
-    <div id="Home" className="h-[105vh]">
-      <div
-        className="fixed w-screen"
-        onTouchMove={() => setGoToMainMenu(!goToMainMenu)}
-      >
-        <MainMenu goToMainMenu={goToMainMenu} />
-        <HeroVideo />
-        <DarkGradients /> {/* <-- must stay on the bottom of the component */}
-      </div>
-      <div className="bg-dark scrollContainer h-[105vh]" />
+    <div id="Home" className="w-screen" onWheel={(e) => triggerMainMenuAnim(e)}>
+      <MainMenu goToMainMenu={goToMainMenu} />
+      <HeroVideo />
+      <DarkGradients /> {/* <-- must stay on the bottom of the component */}
     </div>
   );
 };
