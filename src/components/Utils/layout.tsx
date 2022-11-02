303e056989dcb6fc5e581e2ec0021c00024e586e
Serotoninene <pujol.alexandre@hotmail.fr>
2022-11-02 12:50:18 +0100

    removed attempts at fix the useless rerenders

--- src/components/Utils/layout.tsx
+++ src/components/Utils/layout.tsx
@@ -13,28 +13,26 @@ type Props = {
 
 const Layout = ({ children }: Props): JSX.Element => {
   const { width } = useWindowSize();
-  const memoizedJSX = useMemo(() => {
-    return (
-      <div id="App" className="relative">
-        <Head>
-          <title>Virgile Hasselmann</title>
-          <meta
-            name="description"
-            content="Virgile Hasselmann, a video and photo portfolio"
-          />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        {width! > 768 && <CustomCursor />}
-        <header className="fixed w-screen z-50">
-          <Navbar />
-        </header>
-        <main className="min-h-screen">
-          <div>{children}</div>
-        </main>
-      </div>
-    );
-  }, []);
-  return memoizedJSX;
+
+  return (
+    <div id="App" className="relative">
+      <Head>
+        <title>Virgile Hasselmann</title>
+        <meta
+          name="description"
+          content="Virgile Hasselmann, a video and photo portfolio"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      {width! > 768 && <CustomCursor />}
+      <header className="fixed w-screen z-50">
+        <Navbar />
+      </header>
+      <main className="min-h-screen">
+        <div>{children}</div>
+      </main>
+    </div>
+  );
 };
 
 export default Layout;
