3038163f17637cd602af67a93e4185081a8e9cfc
Serotoninene <pujol.alexandre@hotmail.fr>
2022-11-02 17:47:42 +0100

    trpc request to display the video

--- src/pages/videos/[id].tsx
+++ src/pages/videos/[id].tsx
@@ -1,9 +1,7 @@
-import { useState } from "react";
-import { GetStaticProps } from "next";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 // Server
 import { Video } from "@prisma/client";
-import { prisma } from "@server/prisma";
 import { trpc } from "@server/utils/trpc";
 // Framer motion
 import { motion } from "framer-motion";
@@ -12,23 +10,29 @@ interface Props {}
 
 const VideoPlayer = () => {
   const { query } = useRouter();
-  const id = Number(query.id);
-  // const queryVideo = trpc.video.byId.useQuery(BigInt(810418995343785986));
+  const videoData = trpc.video.byId.useQuery(query.id).data;
+  const [video, setVideo] = useState<Video | null | undefined>(null);
+
+  useEffect(() => {
+    setVideo(videoData);
+  }, [videoData]);
 
   return (
     <motion.div className="h-screen w-screen bg-slate-300 z-20">
-      <video
-        preload="metadata"
-        loop
-        autoPlay
-        muted
-        className={`w-full h-full object-cover border`}
-      >
-        <source
-          src={`https://virgile-portfollio.s3.amazonaws.com/videos/heroVideo.mp4`}
-          type={`video/mp4`}
-        />
-      </video>
+      {video && (
+        <video
+          preload="metadata"
+          loop
+          autoPlay
+          muted
+          className={`w-full h-full object-cover border`}
+        >
+          <source
+            src={`https://virgile-portfollio.s3.amazonaws.com/videos/${video.videoName}`}
+            type={`video/mp4`}
+          />
+        </video>
+      )}
     </motion.div>
   );
 };
