c454c14a20dc7c8e54a5801589b42d80bbd22d61
Serotoninene <pujol.alexandre@hotmail.fr>
2022-11-03 15:00:06 +0100

    introduced the anim flow on the photos page + add the photos part to the seed script

--- src/pages/photos.tsx
+++ src/pages/photos.tsx
@@ -1,27 +1,39 @@
 import React, { useState } from "react";
 // Framer motion
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 // Component
 import useWindowSize from "@src/hooks/useWindowSize";
 
 const videoLink = "https://virgile-portfollio.s3.amazonaws.com/photos/";
 
 export default function photos() {
-  const { width } = useWindowSize();
   const [isOverview, setIsOverview] = useState(false);
   const [category, setCategory] = useState("Artistiques");
   const [photoDisplayed, setPhotoDisplayed] = useState("girl_portrait.png");
 
+  const handleClick = () => {
+    photoDisplayed === "hands_holding.png"
+      ? setPhotoDisplayed("girl_portrait.png")
+      : setPhotoDisplayed("hands_holding.png");
+  };
+
   return (
-    <div className="h-screen pt-14 px-2 flex flex-col justify-between relative sm:px-6">
+    <div
+      className="h-screen pt-14 px-2 flex flex-col justify-between relative sm:px-6"
+      onClick={handleClick}
+    >
       <div className="h-full relative overflow-hidden flex items-center">
-        <motion.img
-          initial={{ y: "-100%" }}
-          animate={{ y: 0 }}
-          transition={{ duration: 0.2, ease: "easeOut" }}
-          src={videoLink + photoDisplayed}
-          className="w-full object-center sm:w-fit sm:h-full"
-        />
+        <AnimatePresence mode="wait">
+          <motion.img
+            key={photoDisplayed}
+            initial={{ y: "-100%" }}
+            animate={{ y: 0 }}
+            exit={{ y: "100%" }}
+            transition={{ duration: 0.2, ease: "easeOut" }}
+            src={videoLink + photoDisplayed}
+            className="w-full sm:w-fit sm:h-full"
+          />
+        </AnimatePresence>
       </div>
       <div className="pt-2 pb-1 flex justify-between text-sm 2xl:text-base">
         <p className="font-light">Made by @Serotoninene, 2022</p>
