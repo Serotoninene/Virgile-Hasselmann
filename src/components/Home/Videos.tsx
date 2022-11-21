b1c9d698f65284b1c95f1c1359cf3798c8475698
Serotoninene <pujol.alexandre@hotmail.fr>
2022-11-21 10:45:57 +0100

    added a line between the two video columns

--- src/components/Home/Videos.tsx
+++ src/components/Home/Videos.tsx
@@ -12,6 +12,7 @@ export default function Videos({ videos }: Props) {
   return (
     <div className="relative py-4 grid grid-cols-2">
       <VideosCol category="Evenements" videos={videos} />
+      <div className="hidden sm:block absolute top-20 h-[90%] w-[0.5px] bg-light left-[50%]"></div>
       <VideosCol category="Clips" videos={videos} />
     </div>
   );
