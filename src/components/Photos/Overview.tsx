3273eeeb2235f3917ce6d77586385bc10cd0fb7d
Serotoninene <pujol.alexandre@hotmail.fr>
2022-11-22 17:00:55 +0100

    set up the miniature component

--- src/components/Photos/Overview.tsx
+++ src/components/Photos/Overview.tsx
@@ -7,21 +7,33 @@ interface Props {
   photos: Photo[];
 }
 
+interface MiniatureProps {
+  photo: Photo;
+  imgWidth: number;
+  imgHeight: number;
+}
+
 const photoLink = "https://virgile-portfollio.s3.amazonaws.com/photos/";
+
+const Miniature = ({ photo, imgWidth, imgHeight }: MiniatureProps) => {
+  return (
+    <Image
+      src={photoLink + photo.photoName}
+      width={imgWidth}
+      height={imgHeight}
+      objectFit="cover"
+    />
+  );
+};
 
 export default function Overview({ photos }: Props) {
   const [imgWidth, setImgWidth] = useState<number>(111);
   const [imgHeight, setImgHeight] = useState<number>(158);
 
   return (
-    <div className="absolute w-[111px] height-screen overflow-scroll right-0">
+    <div className="absolute w-[111px] height-screen right-0">
       {photos.map((p) => (
-        <Image
-          src={photoLink + photos[0].photoName}
-          width={imgWidth}
-          height={imgHeight}
-          objectFit="cover"
-        />
+        <Miniature photo={p} imgWidth={imgWidth} imgHeight={imgHeight} />
       ))}
     </div>
   );
