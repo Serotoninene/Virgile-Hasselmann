1c2bd7b0c833ec141e4b81b0ed228df3fd2ee691
Serotoninene <pujol.alexandre@hotmail.fr>
2022-12-25 14:25:16 +0100

    trying the js way

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,12 +1,24 @@
-import React from "react";
+import useWindowSize from "@src/hooks/useWindowSize";
+import React, { useEffect, useMemo } from "react";
 
 type Props = {};
 
 export default function Test({}: Props) {
+  const { height } = useWindowSize();
+
+  // Calculate 1vh value in pixels
+  // based on window inner height
+  const vh = useMemo(() => (height ? height * 0.01 : 0), [height]);
+  useEffect(() => {
+    if (!height) return;
+    const vh = height * 0.01;
+    document.documentElement.style.setProperty("--vh", vh + "px");
+  }, [height]);
+
   return (
     <div id="Test">
       <p className="text-center">top</p>
-      <p className="fixed bottom-0 text-center">bottom</p>
+      <p className="absolute bottom-0 text-center">bottom</p>
     </div>
   );
 }
