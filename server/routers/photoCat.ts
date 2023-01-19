437014bc6bdeb13b84cd7de08cd3b73d8a922d49
Serotoninene <pujol.alexandre@hotmail.fr>
2023-01-19 17:11:02 +0100

    deleted videoCat and photoCat

--- server/routers/photoCat.ts
+++
@@ -1,9 +0,0 @@
-import { prisma } from "@server/prisma";
-import { publicProcedure, router } from "../trpc";
-
-export const photoCatRouter = router({
-  // Read
-  list: publicProcedure.query(async () => {
-    return await prisma.photo_Category.findMany();
-  }),
-});
