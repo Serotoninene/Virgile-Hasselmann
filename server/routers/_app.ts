15590c1551aaaecc3ba06b005c1dc9cb72b4ca41
Serotoninene <pujol.alexandre@hotmail.fr>
2023-01-19 17:14:32 +0100

    removed the dead router from the app router

--- server/routers/_app.ts
+++ server/routers/_app.ts
@@ -1,16 +1,12 @@
 import { router } from "../trpc";
 import { videoRouter } from "./video";
-import { vidCatRouter } from "./vidCat";
 import { photoRouter } from "./photo";
-import { photoCatRouter } from "./photoCat";
 import { userRouter } from "./user";
 
 export const appRouter = router({
   user: userRouter,
   video: videoRouter,
-  vidCat: vidCatRouter,
   photo: photoRouter,
-  photoCat: photoCatRouter,
 });
 
 // export type definition of API
