0e365da561589107cdda63d034ba4174f9e65597
Serotoninene <pujol.alexandre@hotmail.fr>
2023-11-23 17:06:49 +0100

    refactored with map() function

--- src/components/Admin/VideoInputs.tsx
+++ src/components/Admin/VideoInputs.tsx
@@ -9,12 +9,58 @@ import { Video } from "@prisma/client";
 import Button from "../Utils/Button";
 import { VideoInputsProps } from "types";
 
-interface Props {
-  data?: Video;
+interface Fields {
+  label: string;
+  type: string;
+  placeholder?: string;
+  customClass?: string;
+  name:
+    | "title"
+    | "placeholder_hq"
+    | "videoLink"
+    | "dateOfCreation"
+    | "isSecret";
 }
 
-const VideoInputs = ({ data }: Props) => {
-  const { register, handleSubmit, reset } = useForm<VideoInputsProps>();
+const fields: Fields[] = [
+  {
+    label: "Titre de la photo",
+    type: "text",
+    placeholder: "Titre",
+    name: "title",
+  },
+  {
+    label: "Photo de la vignette",
+    type: "file",
+    placeholder: "Placeholder HQ",
+    name: "placeholder_hq",
+  },
+  {
+    label: "Lien Youtube",
+    type: "text",
+    placeholder: "Lien Youtube",
+    name: "videoLink",
+  },
+  {
+    label: "Date of Creation",
+    type: "date",
+    name: "dateOfCreation",
+  },
+  {
+    label: "Secret ?",
+    type: "checkbox",
+    name: "isSecret",
+    customClass: "w-6 h-6",
+  },
+];
+
+const VideoInputs = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<VideoInputsProps>();
   const [status, setStatus] = useState({ type: "", message: "" });
 
   // trpc  API routes
@@ -63,68 +109,31 @@ const VideoInputs = ({ data }: Props) => {
       onSubmit={handleSubmit(onSubmit)}
       className="my-10 grid grid-cols-1 gap-y-4 gap-x-8 sm:grid-cols-3"
     >
-      <div>
-        <label
-          htmlFor="title"
-          className="block font-medium leading-6 text-gray-900"
-        >
-          Titre de la photo
-        </label>
-        <div className="mt-2">
-          <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-            <input
-              className="block flex-1 border-0 bg-transparent py-2 pl-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-base sm:leading-6"
-              type="text"
-              placeholder="Titre"
-              {...register("title", { required: true })}
-            />
+      {fields.map((field) => (
+        <div key={field.name}>
+          <label
+            htmlFor={field.name}
+            className="block font-medium leading-6 text-gray-900"
+          >
+            {field.label}
+          </label>
+          <div className="mt-2">
+            <div
+              className={`flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md ${field.customClass}`}
+            >
+              <input
+                className="block flex-1 border-0 bg-transparent py-2 pl-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-base sm:leading-6"
+                type={field.type}
+                placeholder={field.placeholder}
+                {...register(field.name, { required: "Ce champ est requis" })}
+              />
+            </div>
+            {errors[field.name] && (
+              <p className="text-red-500">{errors[field.name]?.message}</p>
+            )}
           </div>
         </div>
-      </div>
-
-      <div>
-        <label
-          htmlFor="photo"
-          className="block font-medium leading-6 text-gray-900"
-        >
-          Photo de la vignette
-        </label>
-        <div className="mt-2 flex items-center gap-x-3">
-          <input
-            className="p-2 bg-transparent rounded outline-none border border-gray-500 text-gray-500 cursor-pointer"
-            type="file"
-            placeholder="Placeholder HQ"
-            {...register("placeholder_hq", { required: true })}
-          />
-        </div>
-      </div>
-      <div className="videoLinkContainer">
-        <label htmlFor="date">Lien Youtube</label>
-        <div className="mt-2">
-          <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-            <input
-              className="block flex-1 border-0 bg-transparent py-2 pl-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-base sm:leading-6"
-              type="text"
-              placeholder="Lien Youtube"
-              {...register("videoLink", { required: true })}
-            />
-          </div>
-        </div>
-      </div>
-
-      <div className="titleContainer col-start-2">
-        <label htmlFor="date">Date of Creation</label>
-        <input
-          className="block mt-2 py-2 px-3 bg-transparent drop-shadow cursor-pointer rounded outline-none border border-gray-900  focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600"
-          type="date"
-          {...register("dateOfCreation", { required: true })}
-        />
-      </div>
-
-      <div className="placeholderContainer flex items-center gap-4">
-        <label htmlFor="secret"> Secret ? </label>
-        <input type="checkbox" {...register("isSecret")} />
-      </div>
+      ))}
 
       <div className="flex justify-center">
         <Button>Submit</Button>
